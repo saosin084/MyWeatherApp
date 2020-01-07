6991ddae39257fbce6d62a1cd724c4f381dd8fb2
sventouz <sventouz@gmail.com>
2020-01-07 15:01:29 +0900

    Initial Commit

---
+++ MyWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by higuchiryunosuke on 2020/01/07.
+//  Copyright © 2020年 higuchiryunosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
