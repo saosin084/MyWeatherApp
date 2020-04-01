3ec640b9f64574ebb1349bf3095c54d2445d607f
Yuta <yuta5196@gmail.com>
2020-04-01 18:16:23 +0900

    Display temperature

--- MyWeatherApp/ViewController.swift
+++ MyWeatherApp/ViewController.swift
@@ -14,26 +14,63 @@ class ViewController: UIViewController{
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var kenlabel: UILabel!
+    @IBOutlet weak var templabel: UILabel!
     
     let apiKey = "e846088f82debcd7f279c5792dceb51b"
     var lat = 34.45
     var lon = 132.47
+    var lat1 = 35.69
+    var lon1 = 139.69
+    var lat2 = 34.69
+    var lon2 = 135.5
     var argString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+        if argString == "広島"{
+            Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
         
-        response in
+                response in
         
-         if let responseStr = response.result.value {
-            let jsonResponse = JSON(responseStr)
-            let jsonWeather = jsonResponse["weather"].array![0]
+                if let responseStr = response.result.value {
+                    let jsonResponse = JSON(responseStr)
+                    let jsonWeather = jsonResponse["weather"].array![0]
+                    let jsonTemp = jsonResponse["main"]
+                    
+                    self.label.text = jsonWeather["main"].stringValue
+                    self.templabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                }
+            }
+        }
+        else if argString == "東京"{
+            Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat1)&lon=\(lon1)&appid=\(apiKey)&units=metric").responseJSON {
+        
+                response in
+        
+                if let responseStr = response.result.value {
+                    let jsonResponse = JSON(responseStr)
+                    let jsonWeather = jsonResponse["weather"].array![0]
+                    let jsonTemp = jsonResponse["main"]
+                    self.label.text = jsonWeather["main"].stringValue
+                    self.templabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                }
+            }
+        }
+        else if argString == "大阪"{
+            Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat2)&lon=\(lon2)&appid=\(apiKey)&units=metric").responseJSON {
+        
+                response in
+        
+                if let responseStr = response.result.value {
+                    let jsonResponse = JSON(responseStr)
+                    let jsonWeather = jsonResponse["weather"].array![0]
+                    let jsonTemp = jsonResponse["main"]
             
-            self.label.text = jsonWeather["main"].stringValue
-         }
-         }
-        
+                    self.label.text = jsonWeather["main"].stringValue
+                    self.templabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                }
+            }
+        }
         kenlabel.text = argString
     }
 }
